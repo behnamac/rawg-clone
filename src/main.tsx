2600c28fc3b8747e9b68cdadce75a0b3d91f2140
Behnam Sepehri <behnam.sep@gmail.com>
2025-10-11 20:05:00 +0200

    Update dependencies in package.json and package-lock.json; upgrade Vite to 7.1.9 and various Babel packages to latest versions for improved performance and compatibility.

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ChakraProvider>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </ChakraProvider>
   </StrictMode>
 );
